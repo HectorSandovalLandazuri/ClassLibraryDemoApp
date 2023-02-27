3fc5b870d57db8786e31874fc0a10105649fadfe
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-02-27 12:01:12 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CoreLibrary;
+
+Generators generators = new Generators();
+PersonModel person =new PersonModel
+{
+    Prefix="Mr.",
+    FirstName="Héctor",
+    LastName="Sandoval"
+};
+string message = generators.WelcomeMessage(person.Prefix, person.LastName);
+
+Console.WriteLine(message);
+
